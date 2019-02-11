730d5d32504d176be0cbd4c0fcc26d6744c8fc1c
Nikita <lerowhou@gmail.com>
2019-02-11 21:47:38 +0200

    ..

---
+++ app.js
@@ -0,0 +1,5 @@
+let userScore = document.getElementById('user');
+let compScore = document.getElementById('comp');
+let rock = document.getElementById('rock');
+let paper = document.getElementById('paper');
+let scissors = document.getElementById('scissors');
